d2e67c8edaa995db4188f9c896c672ff8cf0d557
Junseok <narazzo@naver.com>
2022-10-30 04:31:18 +0900

    Style Components

--- src/App.js
+++ src/App.js
@@ -1,5 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import StyledButton from "./components/StyledButton";
+import StyledA from "./components/StyledA";
+import styled from "styled-components";
+
+const PrimaryStyledButton = styled(StyledButton)`
+  background: palevioletred;
+  color: white;
+`;
+
+const UppercaseButton = (props) => (
+  <button {...props} children={props.children.toUpperCase()} />
+);
 
 function App() {
   return (
@@ -7,16 +19,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          <StyledButton>버튼</StyledButton>
+          <StyledButton primary>버튼</StyledButton>
+          <PrimaryStyledButton>버튼</PrimaryStyledButton>
+          <StyledButton as="a" href="/">
+            버튼
+          </StyledButton>
+          <StyledButton as={UppercaseButton}>버튼</StyledButton>
+          <StyledA href="https://google.com">태그</StyledA>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
