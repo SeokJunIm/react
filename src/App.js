4691b91124cf1f68dca7c7e55a80fefc1f06f177
Junseok <narazzo@naver.com>
2022-11-01 02:28:25 +0900

    ControlledComnent

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import "./App.css";
 import StyledButton from "./components/StyledButton";
 import StyledA from "./components/StyledA";
 import styled from "styled-components";
+import { GithubOutlined } from "@ant-design/icons";
+import ControlledComponent from "./components/ControlledComponent";
+import UnControlledComponent from "./components/UncontrolledCompoenet";
 
 const PrimaryStyledButton = styled(StyledButton)`
   background: palevioletred;
@@ -18,16 +21,8 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          <StyledButton>버튼</StyledButton>
-          <StyledButton primary>버튼</StyledButton>
-          <PrimaryStyledButton>버튼</PrimaryStyledButton>
-          <StyledButton as="a" href="/">
-            버튼
-          </StyledButton>
-          <StyledButton as={UppercaseButton}>버튼</StyledButton>
-          <StyledA href="https://google.com">태그</StyledA>
-        </p>
+        <ControlledComponent />
+        <UnControlledComponent />
       </header>
     </div>
   );
